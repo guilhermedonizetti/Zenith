07821bd385eed9ddd17f64778832c3a98a79be24
SACRIER <61637378+SACRIER@users.noreply.github.com>
2020-12-06 17:20:55 -0300

    ..

--- dashboard.php
+++ dashboard.php
@@ -6,12 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
+    <link rel="icon" href="img/icon-na.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/css.css">
     <link rel="stylesheet" href="css/nav.css">
    
-  <link rel="icon" href="https://getbootstrap.com.br/favicon.ico">
+
   <script src="https://kit.fontawesome.com/8786c39b09.js" crossorigin="anonymous"></script>
   <title>ZENITH</title>
 
